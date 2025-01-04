865fe528d2daa6035ec6d9d8dfc376383e20ef7c
Rowee Andrew Apor <roweeapor@gmail.com>
2025-01-05 00:32:25 +0800

    initial dynamic button component and header navlinks adjustment

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
+import Button from '@/components/ui/buttons/button'
+
 export default function Home() {
     return (
         <section>
             <div className='max-w-7xl mx-auto px-4 py-8 flex flex-col justify-center items-center'>
                 <h2>Home page</h2>
+                <Button variant='primary' size='base'>
+                    Button
+                </Button>
             </div>
         </section>
     )
--- components/header/main-header.tsx
+++ components/header/main-header.tsx
@@ -1,6 +1,13 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { NAV_LINKS } from '@/constant/nav-links'
 
 const MainHeader = () => {
+    const pathname = usePathname()
+
     return (
         <header>
             <div className='flex flex-row justify-between items-center max-w-7xl mx-auto px-4 py-8'>
@@ -11,11 +18,25 @@ const MainHeader = () => {
                 </div>
                 <nav>
                     <ul className='flex flex-row space-x-6'>
-                        <li className='px-2 py-1'>
-                            <Link href='/'>Home</Link>
-                        </li>
-                        <li className='px-2 py-1'>
-                            <Link href='/components'>Components</Link>
+                        {NAV_LINKS.map((link) => (
+                            <li
+                                key={link.href}
+                                className={`px-2 py-1 ${pathname === link.href ? 'font-bold text-cyan-500' : 'hover:underline'} `}
+                            >
+                                <Link href={link.href}>{link.label}</Link>
+                            </li>
+                        ))}
+                        <li>
+                            <button
+                                // onClick={() => {
+                                //     document.documentElement.classList.toggle(
+                                //         'dark'
+                                //     )
+                                // }}
+                                className='px-2 py-1 hover:underline'
+                            >
+                                Toggle Dark Mode
+                            </button>
                         </li>
                     </ul>
                 </nav>
